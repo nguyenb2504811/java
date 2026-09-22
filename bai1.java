45359d5410d0f9d011f78bd1f206dbce4810fc99
nguyenb2504811 <nguyenb2504811@student.ctu.edu.vn>
2026-09-22 22:29:40 +0700

    Implement character input loop in Main class
    Added a main class that reads characters from input until 'q' is entered.

--- bai1.java
+++ bai1.java
@@ -1,5 +1,22 @@
 package baitap;
 
-public class bai1 {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char ch;
+
+        do {
+            System.out.print("Nhap vao mot ky tu: ");
+            // Đọc một chuỗi từ bàn phím và lấy ký tự đầu tiên [0]
+            ch = scanner.next().charAt(0);
+
+            System.out.println("Ky tu vua nhap: " + ch);
+
+        } while (ch != 'q'); // Tiếp tục lặp nếu ký tự nhập vào khác 'q'
+
+        System.out.println("Chuong trinh ket thuc.");
+        scanner.close();
+    }
 }
